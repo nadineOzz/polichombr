d39dc2acf523fc8bbe8b8c5125b74d77064efd3b
Nadine Ouaziz <71225818+nadineOzz@users.noreply.github.com>
2021-05-02 17:03:05 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,17 +6,17 @@ git submodule init metasm # or clone github.com/jjyg/metasm
 git submodule update metasm
 
 #Create & activate the virtual environment
-python3 -m venv flask
-. flask/bin/activate
+virtualenv flask
+source flask/bin/activate
 
 #Install packages
 pip install -r requirements.txt 
 
 #Modify the conf.py file
-sed -i '/SQLALCHEMY_DATABASE_URI/c\SQLALCHEMY_DATABASE_URI = "sqlite:////home/kali/polichombr/opt/data/app.db"' ./polichombr/polichombr/config.py
-sed -i '/STORAGE_PATH/c\STORAGE_PATH = "/home/kali/polichombr/opt/data/storage"' ./polichombr/polichombr/config.py
+sed -i '/SQLALCHEMY_DATABASE_URI/c\SQLALCHEMY_DATABASE_URI = "sqlite:////home/kali/polichombr/opt/data/app.db"' /home/kali/polichombr/opt/polichombr/polichombr/config.py
+sed -i '/STORAGE_PATH/c\STORAGE_PATH = "/home/kali/polichombr/opt/data/storage"' /home/kali/polichombr/opt/polichombr/polichombr/config.py
 
-#mkdir /opt/data
+mkdir /opt/data
 
 mv /opt/polichombr/examples/db_create.py .
 python db_create.py
