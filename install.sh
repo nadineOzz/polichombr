d792b54cc9f79e4b5323a0644c17319fde727571
Nadine Ouaziz <71225818+nadineOzz@users.noreply.github.com>
2021-05-04 16:37:44 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,8 +13,8 @@ pip3 install wheel
 pip3 install -r requirements.txt
 
 #Modify the conf.py file
-sed -i '/SQLALCHEMY_DATABASE_URI/c\SQLALCHEMY_DATABASE_URI = "sqlite:////opt/data/app.db"' /polichombr/config.py
-sed -i '/STORAGE_PATH/c\STORAGE_PATH = "/opt/data/storage"' /polichombr/config.py
+sed -i '/SQLALCHEMY_DATABASE_URI/c\SQLALCHEMY_DATABASE_URI = "sqlite:////opt/data/app.db"' polichombr/config.py
+sed -i '/STORAGE_PATH/c\STORAGE_PATH = "/opt/data/storage"' polichombr/config.py
 
 mv /opt/polichombr/examples/db_create.py .
 python3 db_create.py
