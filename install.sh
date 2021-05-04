57413bf2d1f8e6cf533ba4a8815f51abf08dd0be
Nadine Ouaziz <71225818+nadineOzz@users.noreply.github.com>
2021-05-04 14:49:28 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,27 +1,24 @@
 #!/bin/bash
 
-#Prerequisites, on a ubuntu* distribution.
-#sudo apt-get install -y git virtualenv ruby libffi-dev python-dev graphviz gcc libssl-dev python-pip
+#Install packages & metasm git
 git submodule init metasm # or clone github.com/jjyg/metasm
 git submodule update metasm
-
-#Create & activate the virtual environment
-
-python3 -m venv flask
-source flask/bin/activate
-
-#Install packages
-pip3 install wheel
-pip3 install -r requirements.txt 
+pip3 install wheel 
+pip3 install -r requirements.txt
 
 #Modify the conf.py file
 sed -i '/SQLALCHEMY_DATABASE_URI/c\SQLALCHEMY_DATABASE_URI = "sqlite:////opt/data/app.db"' polichombr/config.py
 sed -i '/STORAGE_PATH/c\STORAGE_PATH = "/opt/data/storage"' polichombr/config.py
 
-mkdir /opt/data
+#Marshmallow library updates
+sed -i '11ifrom marshmallow import Schema' polichombr/models/user.py polichombr/models/analysis.py polichombr/models/sample.py polichombr/models/idaactions.py polichombr/models/yara_rule.py polichombr/models/family.py 
+sed -i 's/ma.ModelSchema/Schema/g' polichombr/models/user.py polichombr/models/analysis.py polichombr/models/sample.py polichombr/models/idaactions.py polichombr/models/yara_rule.py polichombr/models/family.py
 
-mv /opt/polichombr/examples/db_create.py .
-python db_create.py
+#Werkzeug library updates
+sed -i 's/from werkzeug import secure_filename/from werkzeug.utils import secure_filename/g' polichombr/views/webui_families.py polichombr/views/webui_sample.py
+
+cp ./examples/db_create.py .
+python3 db_create.py
 
 
 
