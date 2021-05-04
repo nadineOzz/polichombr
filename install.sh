2f492e60ff4c86bfa8d3ec1d1811a3560eed711b
Nadine Ouaziz <71225818+nadineOzz@users.noreply.github.com>
2021-05-04 18:36:38 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,17 +6,21 @@ git submodule update metasm
 
 
 #Create virtual environment & activate it 
-python3 -m venv flask
-source flask/bin/activate
+#python3 -m venv flask
+#source flask/bin/activate
 
 pip3 install wheel 
 pip3 install -r requirements.txt
 
-#Modify the conf.py file
-sed -i '/SQLALCHEMY_DATABASE_URI/c\SQLALCHEMY_DATABASE_URI = "sqlite:///opt\data\app.db"' ./polichombr/config.py
-sed -i '/STORAGE_PATH/c\STORAGE_PATH = "/opt/data/storage"' ./polichombr/config.py
+#Create an environment variable
+install_path=$(cd .. && pwd)
+echo $install_path
 
-mv /opt/polichombr/examples/db_create.py .
+#Modify the conf.py file
+sed -i "/SQLALCHEMY_DATABASE_URI/c\SQLALCHEMY_DATABASE_URI =\"sqlite:////$install_path/data/app.db\"" ./polichombr/config.py
+sed -i "/STORAGE_PATH/c\STORAGE_PATH =\"$install_path/data/storage\"" ./polichombr/config.py
+
+cp ./examples/db_create.py .
 python3 db_create.py
 
 
