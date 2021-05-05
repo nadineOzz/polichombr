f91044d46042ae9d83e2758c540356e832196c2f
Nadine Ouaziz <71225818+nadineOzz@users.noreply.github.com>
2021-05-05 11:40:04 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,18 +4,12 @@
 git submodule init metasm # or clone github.com/jjyg/metasm
 git submodule update metasm
 
-
-#Create virtual environment & activate it 
-#python3 -m venv flask
-#source flask/bin/activate
-
+#Install requirements
 pip3 install wheel 
 pip3 install -r requirements.txt
 
 #Create an environment variable
 install_path=$(cd .. && pwd)
-echo $install_path
-
 #Modify the conf.py file
 sed -i "/SQLALCHEMY_DATABASE_URI/c\SQLALCHEMY_DATABASE_URI = \"sqlite:////$install_path/data/app.db\"" ./polichombr/config.py
 sed -i "/STORAGE_PATH/c\STORAGE_PATH =\"$install_path/data/storage\"" ./polichombr/config.py
