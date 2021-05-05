16657e5c8475049f360fe3cc8be683865ce95ea9
Nadine Ouaziz <71225818+nadineOzz@users.noreply.github.com>
2021-05-05 15:10:01 +0200

    Update installd.sh

--- installd.sh
+++ installd.sh
@@ -1,24 +1,22 @@
+
 #!/bin/bash
 
-#Install packages & metasm git
+#Prerequisites, on a ubuntu* distribution.
+#sudo apt-get install -y git virtualenv ruby libffi-dev python-dev graphviz gcc libssl-dev python-pip
 git submodule init metasm # or clone github.com/jjyg/metasm
 git submodule update metasm
 
-pip3 install wheel
-pip3 install -r requirements.txt
- 
+#Create & activate the virtual environment
 
-#Install requirements
-pip3 install wheel 
+python -m venv flask
+pip3 install wheel
 pip3 install -r requirements.txt
 
 #Modify the conf.py file
 sed -i '/SQLALCHEMY_DATABASE_URI/c\SQLALCHEMY_DATABASE_URI = "sqlite:////opt/data/app.db"' polichombr/config.py
 sed -i '/STORAGE_PATH/c\STORAGE_PATH = "/opt/data/storage"' polichombr/config.py
 
-#sed -i '11ifrom marshmallow import Schema' polichombr/models/user.py polichombr/models/analysis.py polichombr/models/sample.py polichombr/models/idaactions.py polichombr/models/yara_rule.py polichombr/models/family.py 
-#sed -i 's/ma.ModelSchema/Schema/g' polichombr/models/user.py polichombr/models/analysis.py polichombr/models/sample.py polichombr/models/idaactions.py polichombr/models/yara_rule.py polichombr/models/family.py
-#sed -i 's/from werkzeug import secure_filename/from werkzeug.utils import secure_filename/g' polichombr/views/webui_families.py polichombr/views/webui_sample.py
+mkdir /opt/data
 
-cp ./examples/db_create.py .
-python3 db_create.py
+mv /opt/polichombr/examples/db_create.py .
+#python db_create.py
