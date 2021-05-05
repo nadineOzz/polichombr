d00b4a9d83ec4f91149b80ad10556ac07ce45753
Nadine Ouaziz <71225818+nadineOzz@users.noreply.github.com>
2021-05-05 15:55:30 +0200

    Update dockerinstall.sh

--- dockerinstall.sh
+++ dockerinstall.sh
@@ -8,17 +8,15 @@ git submodule update metasm
 
 #Create & activate the virtual environment
 python3 -m venv flask
-source flask/bin/activate
+. flask/bin/activate
 
 #Install packages
 pip3 install wheel
 pip3 install -r requirements.txt
 
 #Create an environment variable
-install_path=$(cd .. && pwd)
-#Modify the conf.py file
-sed -i "/SQLALCHEMY_DATABASE_URI/c\SQLALCHEMY_DATABASE_URI = \"sqlite:////$install_path/data/app.db\"" polichombr/config.py
-sed -i "/STORAGE_PATH/c\STORAGE_PATH =\"$install_path/data/storage\"" polichombr/config.py
+sed -i '/SQLALCHEMY_DATABASE_URI/c\SQLALCHEMY_DATABASE_URI = "sqlite:////opt/data/app.db"' polichombr/config.py
+sed -i '/STORAGE_PATH/c\STORAGE_PATH = "/opt/data/storage"' polichombr/config.py
 
 cp ./examples/db_create.py .
 python3 db_create.py
