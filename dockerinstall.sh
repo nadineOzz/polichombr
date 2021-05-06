1deaab2636213a76b40afe49397dce1481237b4c
Nadine Ouaziz <71225818+nadineOzz@users.noreply.github.com>
2021-05-06 13:53:58 +0200

    Update dockerinstall.sh

--- dockerinstall.sh
+++ dockerinstall.sh
@@ -16,13 +16,12 @@ pip3 install -r requirements.txt
 
 #Create an environment variable
 install_path=$(cd .. && pwd)
+
 #Modify the conf.py file
 sed -i "/SQLALCHEMY_DATABASE_URI/c\SQLALCHEMY_DATABASE_URI = \"sqlite:////$install_path/data/app.db\"" ./polichombr/config.py
 sed -i "/STORAGE_PATH/c\STORAGE_PATH =\"$install_path/data/storage\"" ./polichombr/config.py
 
+#Copy & run the db_create.py in the current directory
 cp ./examples/db_create.py .
 python3 db_create.py
 
-#installd.sh
-#. flask/bin/activate
-#./run.py
